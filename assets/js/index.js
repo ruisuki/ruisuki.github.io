ea2e2ec6d1d3c26b75025a05307e3d50780ca970
RUi <12548014+ruiomoshiroi@user.noreply.gitee.com>
2023-03-22 17:28:20 +0800

    提交文件

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+var wrap = document.getElementsByClassName('wrap')[0];
+var cards = wrap.getElementsByClassName('card')
+
+console.log(wrap);
+
+for (var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('mousemove', function () {
+    removeActive()
+    this.className = "card active"
+  })
+}
+
+function removeActive() {
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].className = "card";
+  }
+}
